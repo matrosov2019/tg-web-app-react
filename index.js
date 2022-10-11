a852017348a72606d013edaf47c0140d3d4ae2a6
Matrosov Dmitry <d.matrosov@vitrina.tv>
2022-10-12 01:28:41 +0300

    Новый репозиторий

---
+++ index.js
@@ -0,0 +1,47 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '5726828664:AAF06baBgCvYtlc5NvYd6fedKaYObIIW2kg';
+
+const webAppUrl = "https://ya.ru";
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo (.+)/, async (msg, match) => {
+
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+
+});
+
+bot.on('message', async (msg) => {
+
+    const chatId = msg.chat.id;
+    const text = msg.text;
+    //const resp = match[1]; // the captured "whatever"
+    if (text === "/start") {
+
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: 'Заполните форму',
+                            web_app: {
+                                url: webAppUrl
+                            }
+                        }
+                    ]
+                ]
+            }
+        });
+
+    }
+
+});
