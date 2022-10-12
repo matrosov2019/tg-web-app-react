b9fbaf84c4494ffc2af1546494a24c552b6e5cdd
Matrosov Dmitry <d.matrosov@vitrina.tv>
2022-10-12 12:51:53 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom';
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
-    const {onToggleButton, tg} = useTelegram();
+    const {tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
@@ -13,7 +16,10 @@ function App() {
     return (
         <div className="App">
             <Header/>
-            <button onClick={onToggleButton}>Toggle</button>
+            <Routes>
+                <Route index element={<ProductList/>}/>
+                <Route path={'form'} element={<Form/>}/>
+            </Routes>
         </div>
     );
 }
